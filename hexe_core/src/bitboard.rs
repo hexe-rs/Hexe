8b6e195cca421e5b96c66390b44a3fc04657e525
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-10-16 12:16:46 -0400

    Implement fmt traits for Bitboard

--- hexe_core/src/bitboard.rs
+++ hexe_core/src/bitboard.rs
@@ -1,5 +1,28 @@
 //! A bitmap chess board representation.
 
+use core::fmt;
+
 /// A mapping of sixty-four bits to squares of a chess board.
 #[derive(Copy, Clone, Hash, PartialEq, Eq)]
 pub struct Bitboard(pub u64);
+
+macro_rules! forward_fmt_impl {
+    ($($f:ident)+) => {
+        $(impl fmt::$f for Bitboard {
+            #[inline]
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                fmt::$f::fmt(&self.0, f)
+            }
+        })+
+    }
+}
+
+forward_fmt_impl! { Binary Octal LowerHex UpperHex }
+
+impl fmt::Debug for Bitboard {
+    #[inline]
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // 2 for "0x" + 16 for number
+        write!(f, "Bitboard({:#018X})", self)
+    }
+}
