ac9945ea0591ce08d79c5326b458a7adb47e779b
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-10-17 15:55:21 -0400

    Implement FromStr for Color

--- hexe_core/src/color.rs
+++ hexe_core/src/color.rs
@@ -1,5 +1,7 @@
 //! A color to represent pieces or board squares.
 
+use core::str::FromStr;
+
 /// A black or white color.
 #[derive(Copy, Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord, FromUnchecked)]
 #[uncon(impl_from, other(u16, u32, u64, usize))]
@@ -9,4 +11,64 @@ pub enum Color {
     White,
     /// Black color.
     Black,
+}
+
+/// The error returned when `Color::from_str` fails.
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+pub struct FromStrError(());
+
+impl FromStr for Color {
+    type Err = FromStrError;
+
+    fn from_str(s: &str) -> Result<Color, FromStrError> {
+        if s.len() > 1 {
+            let (color, exp) = match s.as_bytes()[0] | 32 {
+                b'w' => (Color::White, "hite"),
+                b'b' => (Color::Black, "lack"),
+                _ => return Err(FromStrError(())),
+            };
+            let rem = unsafe { s.get_unchecked(1..) };
+            if rem.len() == 4 {
+                for (a, b) in rem.bytes().zip(exp.bytes()) {
+                    if a | 32 != b {
+                        return Err(FromStrError(()));
+                    }
+                }
+            } else if rem.len() != 0 {
+                return Err(FromStrError(()));
+            }
+            Ok(color)
+        } else {
+            Err(FromStrError(()))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_str() {
+        use self::Color::*;
+
+        static STRINGS: &[(&str, Color)] = &[
+            ("white", White), ("black", Black),
+            ("WHITE", White), ("BLACK", Black),
+            ("wHiTe", White), ("BlAcK", Black),
+        ];
+
+        static FAILS: &[&str] = &[
+            "whit",  "blac",
+            "whits", "block",
+        ];
+
+        for &(s, c) in STRINGS {
+            assert_eq!(s.parse().ok(), Some(c));
+        }
+
+        for &f in FAILS {
+            assert_eq!(Color::from_str(f).ok(), None);
+        }
+    }
 }
