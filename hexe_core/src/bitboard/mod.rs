f611afc5dddbce9d324cef2b23dfda22153f8e52
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-10-17 12:13:00 -0400

    Move bitboard::masks to its own file

--- hexe_core/src/bitboard/mod.rs
+++ hexe_core/src/bitboard/mod.rs
@@ -1,5 +1,6 @@
 //! A bitmap chess board representation.
 
+pub mod masks;
 mod impls;
 
 /// A mapping of sixty-four bits to squares of a chess board.
@@ -12,35 +13,4 @@ impl Bitboard {
 
     /// Black board squares.
     pub const BLACK: Bitboard = Bitboard(0xAA55AA55AA55AA55);
-}
-
-/// Bitboard masks for each file and rank.
-pub mod masks {
-    use super::*;
-
-    macro_rules! impl_consts {
-        ($base:expr, $shift:expr; $cur:ident, $($next:ident),+ $(,)*) => {
-            pub const $cur: Bitboard = Bitboard($base);
-            impl_consts!($shift; $cur, $($next),+);
-        };
-        ($shift:expr; $prev:ident, $cur:ident) => {
-            pub const $cur: Bitboard = Bitboard($prev.0 << $shift);
-        };
-        ($shift:expr; $prev:ident, $cur:ident, $($next:ident),+) => {
-            impl_consts!($shift; $prev, $cur);
-            impl_consts!($shift; $cur, $($next),+);
-        };
-    }
-
-    impl_consts! {
-        0x0101010101010101, 1;
-        FILE_A, FILE_B, FILE_C, FILE_D,
-        FILE_E, FILE_F, FILE_G, FILE_H,
-    }
-
-    impl_consts! {
-        0xFF, 8;
-        RANK_1, RANK_2, RANK_3, RANK_4,
-        RANK_5, RANK_6, RANK_7, RANK_8,
-    }
 }
