33cc65c876ed98e0170073d758a6b7b5f63f226f
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-10-17 10:40:53 -0400

    Create file and rank Bitboard mask constants

--- hexe_core/src/bitboard.rs
+++ hexe_core/src/bitboard.rs
@@ -127,3 +127,34 @@ impl AsMut<Bitboard> for u64 {
         unsafe { &mut *(self as *mut _ as *mut _) }
     }
 }
+
+/// Bitboard masks for each file and rank.
+pub mod masks {
+    use super::*;
+
+    macro_rules! impl_consts {
+        ($base:expr, $shift:expr; $cur:ident, $($next:ident),+ $(,)*) => {
+            pub const $cur: Bitboard = Bitboard($base);
+            impl_consts!($shift; $cur, $($next),+);
+        };
+        ($shift:expr; $prev:ident, $cur:ident) => {
+            pub const $cur: Bitboard = Bitboard($prev.0 << $shift);
+        };
+        ($shift:expr; $prev:ident, $cur:ident, $($next:ident),+) => {
+            impl_consts!($shift; $prev, $cur);
+            impl_consts!($shift; $cur, $($next),+);
+        };
+    }
+
+    impl_consts! {
+        0x0101010101010101, 1;
+        FILE_A, FILE_B, FILE_C, FILE_D,
+        FILE_E, FILE_F, FILE_G, FILE_H,
+    }
+
+    impl_consts! {
+        0xFF, 8;
+        RANK_1, RANK_2, RANK_3, RANK_4,
+        RANK_5, RANK_6, RANK_7, RANK_8,
+    }
+}
