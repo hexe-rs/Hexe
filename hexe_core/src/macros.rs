9b32568c2bcc828d09d1740d0ed56be2726a8206
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-10-18 10:19:07 -0400

    Make macros in 'macros' module portable
    Removed import requirements.

--- hexe_core/src/macros.rs
+++ hexe_core/src/macros.rs
@@ -1,6 +1,6 @@
 macro_rules! forward_bit_ops_impl {
     ($t:ident => $($t1:ident $f1:ident $t2:ident $f2:ident)+) => {
-        $(impl<T: Into<$t>> ops::$t1<T> for $t {
+        $(impl<T: Into<$t>> ::core::ops::$t1<T> for $t {
             type Output = Self;
 
             #[inline]
@@ -9,7 +9,7 @@ macro_rules! forward_bit_ops_impl {
             }
         }
 
-        impl<T: Into<$t>> ops::$t2<T> for $t {
+        impl<T: Into<$t>> ::core::ops::$t2<T> for $t {
             #[inline]
             fn $f2(&mut self, other: T) {
                 (self.0).$f2(other.into().0)
@@ -27,14 +27,14 @@ macro_rules! impl_set_ops {
             BitOr  bitor  BitOrAssign  bitor_assign
         }
 
-        impl<T: Into<$t>> ops::Sub<T> for $t {
+        impl<T: Into<$t>> ::core::ops::Sub<T> for $t {
             type Output = Self;
 
             #[inline]
             fn sub(self, other: T) -> Self { $t(self.0 & !other.into().0) }
         }
 
-        impl<T: Into<$t>> ops::SubAssign<T> for $t {
+        impl<T: Into<$t>> ::core::ops::SubAssign<T> for $t {
             #[inline]
             fn sub_assign(&mut self, other: T) { self.0 &= !other.into().0 }
         })+
