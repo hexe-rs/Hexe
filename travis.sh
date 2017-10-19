dccf701a1ff7fe43e62829bceaaaf4dcd152be70
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-10-18 20:01:57 -0400

    [Travis] Test nightly features

--- travis.sh
+++ travis.sh
@@ -4,6 +4,6 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for d in hexe hexe_core; do
     cd "$DIR/$d"
-    cargo test
-    cargo test --no-default-features
+    cargo test $FEATURES
+    cargo test $FEATURES --no-default-features
 done
