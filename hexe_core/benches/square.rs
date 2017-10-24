44a7597bb35c28b26633deae98ac0b050ea1e9ce
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-10-24 11:14:43 -0400

    Add benchmarks for sliding attacks

---
+++ hexe_core/benches/square.rs
@@ -0,0 +1,23 @@
+#![feature(test)]
+extern crate test;
+extern crate rand;
+extern crate hexe_core;
+
+use test::{Bencher, black_box};
+use hexe_core::prelude::*;
+
+macro_rules! impl_sliding_benches {
+    ($($f:ident)+) => {
+        $(#[bench]
+        fn $f(b: &mut Bencher) {
+            let occ = Bitboard(rand::random());
+            b.iter(|| {
+                for s in Square::all() {
+                    black_box(black_box(s).$f(black_box(occ)));
+                }
+            });
+        })+
+    }
+}
+
+impl_sliding_benches! { rook_attacks bishop_attacks queen_attacks }
