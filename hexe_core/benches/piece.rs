4b3cc3ecc164ab4339d3916b3ea6057c73f57d63
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-10-28 18:34:18 -0400

    Add unoptimized PieceMap benchmarks for comparison

--- hexe_core/benches/piece.rs
+++ hexe_core/benches/piece.rs
@@ -6,6 +6,19 @@ use test::{Bencher, black_box};
 use hexe_core::piece::*;
 use hexe_core::square::Square;
 
+fn find(piece: Piece, map: &map::PieceMap) -> Option<Square> {
+    for (i, &slot) in map.as_bytes().iter().enumerate() {
+        if slot == piece as u8 {
+            return Some(i.into())
+        }
+    }
+    None
+}
+
+fn contains(piece: Piece, map: &map::PieceMap) -> bool {
+    map.as_bytes().contains(&(piece as u8))
+}
+
 #[bench]
 fn map_contains_piece(b: &mut Bencher) {
     let piece = Piece::BlackKing;
@@ -17,12 +30,32 @@ fn map_contains_piece(b: &mut Bencher) {
 }
 
 #[bench]
+fn map_contains_piece_naive(b: &mut Bencher) {
+    let piece = Piece::BlackKing;
+    let mut map = map::PieceMap::new();
+    map.insert(Square::H8, piece);
+    b.iter(|| {
+        black_box(contains(black_box(piece), black_box(&map)));
+    });
+}
+
+#[bench]
 fn map_find(b: &mut Bencher) {
     let piece = Piece::WhiteRook;
     let mut map = map::PieceMap::new();
     map.insert(Square::H8, piece);
     b.iter(|| {
         black_box(black_box(&map).find(black_box(piece)));
+    });
+}
+
+#[bench]
+fn map_find_naive(b: &mut Bencher) {
+    let piece = Piece::WhiteRook;
+    let mut map = map::PieceMap::new();
+    map.insert(Square::H8, piece);
+    b.iter(|| {
+        black_box(find(black_box(piece), black_box(&map)));
     });
 }
 
