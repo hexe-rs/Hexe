65bd136bee6bf3769506a12c4d052d4fca3022e0
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-10-31 10:28:23 -0400

    Add Position::bitboard

--- hexe/src/position.rs
+++ hexe/src/position.rs
@@ -1,6 +1,7 @@
 //! A chess game state position.
 
 use hexe_core::piece::map::PieceMap;
+use prelude::*;
 
 /// A representation of the current game state.
 #[derive(PartialEq, Eq)]
@@ -26,5 +27,66 @@ impl Default for Position {
             pieces: [PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING],
             colors: [WHITE, BLACK],
         }
+    }
+}
+
+impl Position {
+    /// Returns the corresponding bitboard for the retriever.
+    ///
+    /// # Examples
+    ///
+    /// This method can be used for [`Piece`], [`PieceKind`], and [`Color`]:
+    ///
+    /// ```
+    /// # use hexe::position::Position;
+    /// # use hexe::prelude::*;
+    /// let pos = Position::default();
+    ///
+    /// let kind  = PieceKind::Knight;
+    /// let color = Color::White;
+    /// let piece = Piece::new(kind, color);
+    ///
+    /// assert_eq!(
+    ///     pos.bitboard(kind) & pos.bitboard(color),
+    ///     pos.bitboard(piece)
+    /// );
+    /// ```
+    ///
+    /// [`Piece`]:     ../piece/enum.Piece.html
+    /// [`PieceKind`]: ../piece/enum.PieceKind.html
+    /// [`Color`]:     ../color/enum.Color.html
+    #[inline]
+    pub fn bitboard<T: BitboardRetriever>(&self, retr: T) -> Bitboard {
+        retr.bitboard(self)
+    }
+}
+
+/// A type whose instances serve to retrieve a [`Bitboard`] from a [`Position`].
+///
+/// [`Bitboard`]: ../bitboard/struct.Bitboard.html
+/// [`Position`]: struct.Position.html
+pub trait BitboardRetriever {
+    /// Retrieves the corresponding `Bitboard` for `self` from a `Position`.
+    fn bitboard(self, pos: &Position) -> Bitboard;
+}
+
+impl BitboardRetriever for PieceKind {
+    #[inline]
+    fn bitboard(self, pos: &Position) -> Bitboard {
+        Bitboard(pos.pieces[self as usize])
+    }
+}
+
+impl BitboardRetriever for Color {
+    #[inline]
+    fn bitboard(self, pos: &Position) -> Bitboard {
+        Bitboard(pos.colors[self as usize])
+    }
+}
+
+impl BitboardRetriever for Piece {
+    #[inline]
+    fn bitboard(self, pos: &Position) -> Bitboard {
+        self.kind().bitboard(pos) & self.color().bitboard(pos)
     }
 }
