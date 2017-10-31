cefe1e32ea7aaa4f7e2f6130e444fe30256e2c71
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-10-31 11:30:38 -0400

    [Cargo] Add uncon dependency to hexe

--- hexe/src/lib.rs
+++ hexe/src/lib.rs
@@ -6,6 +6,7 @@
 #![doc(html_logo_url = "https://raw.githubusercontent.com/hexe-rs/Hexe/assets/Icon.png")]
 
 extern crate hexe_core;
+extern crate uncon;
 
 pub use hexe_core::{bitboard, castle_rights, color, piece, square};
 
