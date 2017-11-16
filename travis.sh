5772328634da1045267ee4a78549f69ade553f4f
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-11-16 15:48:44 -0500

    [Travis] Attempt to test 32 bit

--- travis.sh
+++ travis.sh
@@ -4,6 +4,11 @@ set -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+if $TEST_32_BIT; then
+    echo "$TEST_32_BIT"
+    curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain="stable-i686-unknown-linux-gnu" -y
+fi
+
 for d in hexe_core hexe; do
     cd "$DIR/$d"
     cargo test $FEATURES
