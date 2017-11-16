b851d00ccec4d2fbc841d96aaba08c792d7cc946
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-11-16 15:50:12 -0500

    [Travis] Use string comparison for 32 bit test

--- travis.sh
+++ travis.sh
@@ -4,8 +4,7 @@ set -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if $TEST_32_BIT; then
-    echo "$TEST_32_BIT"
+if [[ $TEST_32_BIT == "true" ]]; then
     curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain="stable-i686-unknown-linux-gnu" -y
 fi
 
