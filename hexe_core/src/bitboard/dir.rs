60739f0ee6eec4c24344cffe5b7f0b1b191475f3
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-11-19 13:24:36 -0500

    Move Not impl for Direction

--- hexe_core/src/bitboard/dir.rs
+++ hexe_core/src/bitboard/dir.rs
@@ -1,3 +1,5 @@
+use core::ops;
+
 /// A cardinal direction that can be used to shift or fill the bits of a
 /// [`Bitboard`](struct.Bitboard.html).
 #[derive(Copy, Clone, Debug, Hash, PartialEq, Eq)]
@@ -18,4 +20,17 @@ pub enum Direction {
     Northwest,
     /// Southwest (down + left).
     Southwest
+}
+
+impl ops::Not for Direction {
+    type Output = Direction;
+
+    #[inline]
+    fn not(self) -> Direction {
+        use self::Direction::*;
+        static NOT: [Direction; 8] = [
+            South, North, West, East, Southwest, Northwest, Southeast, Northeast
+        ];
+        NOT[self as usize]
+    }
 }
