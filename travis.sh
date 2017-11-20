f13df8df036cefb2357f119ca2fd63a0ad3f18ed
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-11-20 12:00:22 -0500

    [Travis] Test clippy lints against nightly

--- travis.sh
+++ travis.sh
@@ -4,15 +4,29 @@ set -e
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-if [[ -z "$TARGET" ]]; then
-    TARGET_ARGS=""
-else
-    rustup target add "$TARGET"
-    TARGET_ARGS="--target $TARGET"
-fi
+CRATES="hexe_core hexe"
 
-for d in hexe_core hexe; do
-    cd "$DIR/$d"
-    cargo test $TARGET_ARGS $FEATURES
-    cargo test $TARGET_ARGS $FEATURES --no-default-features
-done
+if [[ -n "$CLIPPY" ]]; then
+    if ! cargo install clippy --debug --force; then
+        echo "COULD NOT COMPILE CLIPPY, IGNORING CLIPPY TESTS"
+        exit
+    fi
+
+    for crate in $CRATES; do
+        cd "$DIR/$crate"
+        cargo clippy -- -Dclippy
+    done
+else
+    if [[ -z "$TARGET" ]]; then
+        TARGET_ARGS=""
+    else
+        rustup target add "$TARGET"
+        TARGET_ARGS="--target $TARGET"
+    fi
+
+    for crate in $CRATES; do
+        cd "$DIR/$crate"
+        cargo test $TARGET_ARGS $FEATURES
+        cargo test $TARGET_ARGS $FEATURES --no-default-features
+    done
+fi
