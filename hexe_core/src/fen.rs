f92712d90a30e1ff01e653e18aa37386fccd686a
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-01-01 13:55:54 -0500

    Implement fmt::Display for Fen

--- hexe_core/src/fen.rs
+++ hexe_core/src/fen.rs
@@ -2,6 +2,9 @@
 //!
 //! [fen]: https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
 
+use core::fmt::{self, Write};
+use core::str;
+
 use prelude::*;
 use piece::map::PieceMap;
 
@@ -22,6 +25,37 @@ pub struct Fen {
     pub halfmoves: u32,
     /// The fullmove number.
     pub fullmoves: u32,
+}
+
+impl fmt::Display for Fen {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.pieces.map_fen(|s| f.write_str(s))?;
+
+        {
+            let mut buf = *b"   ";
+            buf[1] = match self.color {
+                Color::White => b'w',
+                Color::Black => b'b',
+            };
+            let string = unsafe { str::from_utf8_unchecked(&buf) };
+            f.write_str(string)?;
+        }
+
+        self.castling.map_str(|s| f.write_str(s))?;
+
+        if let Some(sq) = self.en_passant {
+            f.write_char(' ')?;
+            sq.map_str(|s| f.write_str(s))?;
+            f.write_char(' ')?;
+        } else {
+            f.write_str(" - ")?;
+        }
+
+        self.halfmoves.fmt(f)?;
+        f.write_char(' ')?;
+
+        self.fullmoves.fmt(f)
+    }
 }
 
 impl Fen {
@@ -52,4 +86,23 @@ impl Fen {
         halfmoves: 0,
         fullmoves: 1,
     };
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[cfg(feature = "std")]
+    fn display() {
+        let fens = [
+            (Fen::STARTING, "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"),
+            (Fen::EMPTY,    "8/8/8/8/8/8/8/8 w - - 0 1"),
+        ];
+
+        for &(ref fen, exp) in fens.iter() {
+            let string = format!("{}", fen);
+            assert_eq!(string, exp);
+        }
+    }
 }
