42c3439d8edaf39af90671c358b714b897f91bb9
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-01-14 18:28:35 -0500

    Use ptr export of write_bytes

--- hexe/src/util.rs
+++ hexe/src/util.rs
@@ -1,9 +1,9 @@
 use std::mem;
-use std::intrinsics::write_bytes;
+use std::ptr;
 
 #[inline]
 pub unsafe fn zero<T: ?Sized>(val: &mut T) {
     let len = mem::size_of_val(val);
     let ptr = val as *mut T as *mut u8;
-    write_bytes(ptr, 0, len);
+    ptr::write_bytes(ptr, 0, len);
 }
