bd028a7223ddbd8d2788914bbec9d58688031ff3
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-02-08 17:59:49 -0500

    Make use of Rand impls in square::benches

--- hexe_core/src/square/benches.rs
+++ hexe_core/src/square/benches.rs
@@ -1,11 +1,12 @@
 use super::*;
 use test::{Bencher, black_box};
+use rand::{Rand, self};
 
 macro_rules! impl_sliding_benches {
     ($($f:ident)+) => { $(
         #[bench]
         fn $f(b: &mut Bencher) {
-            let pairs = rand_occ_pairs(1000);
+            let pairs = rand_pairs::<Square, Bitboard>(1000);
             b.iter(|| {
                 for &(sq, occ) in &pairs {
                     black_box(black_box(sq).$f(black_box(occ)));
@@ -17,16 +18,8 @@ macro_rules! impl_sliding_benches {
 
 impl_sliding_benches! { rook_attacks bishop_attacks queen_attacks }
 
-fn rand_square() -> Square {
-    Square::from(::rand::random::<u8>())
-}
-
-fn rand_pairs(n: usize) -> Vec<(Square, Square)> {
-    (0..n).map(|_| (rand_square(), rand_square())).collect()
-}
-
-fn rand_occ_pairs(n: usize) -> Vec<(Square, Bitboard)> {
-    (0..n).map(|_| (rand_square(), Bitboard(::rand::random()))).collect()
+fn rand_pairs<T: Rand, U: Rand>(n: usize) -> Vec<(T, U)> {
+    (0..n).map(|_| (rand::random(), rand::random())).collect()
 }
 
 #[bench]
@@ -58,7 +51,7 @@ fn square_color(b: &mut Bencher) {
 
 #[bench]
 fn square_distance_1000(b: &mut Bencher) {
-    let squares = rand_pairs(1000);
+    let squares = rand_pairs::<Square, Square>(1000);
     b.iter(|| {
         for &(s1, s2) in &squares {
             black_box(black_box(s1).distance(black_box(s2)));
@@ -72,7 +65,7 @@ fn square_distance_normal_1000(b: &mut Bencher) {
         use std::cmp::max;
         max(s1.file().distance(s2.file()), s1.rank().distance(s2.rank()))
     }
-    let squares = rand_pairs(1000);
+    let squares = rand_pairs::<Square, Square>(1000);
     b.iter(|| {
         for &(s1, s2) in &squares {
             black_box(distance(black_box(s1), black_box(s2)));
