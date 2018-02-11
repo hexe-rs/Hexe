295ec94f100a7d2cc3832891ad49ff3ae9396e49
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-02-11 12:46:26 -0500

    Add fallback string implementation to main

--- hexe/src/main.rs
+++ hexe/src/main.rs
@@ -2,6 +2,19 @@ extern crate hexe;
 
 use std::env;
 
+/// A string that defaults to `str::default()` if `None`.
+struct FallbackStr<T>(Option<T>);
+
+impl<T: AsRef<str>> AsRef<str> for FallbackStr<T> {
+    fn as_ref(&self) -> &str {
+        if let Some(ref s) = self.0 {
+            s.as_ref()
+        } else {
+            Default::default()
+        }
+    }
+}
+
 fn main() {
     let mut args = env::args_os();
     let mut eng  = hexe::engine::Engine::default();
@@ -11,7 +24,7 @@ fn main() {
         1 => uci.start(),
         _ => {
             args.next();
-            uci.start_with(args.filter_map(|a| a.into_string().ok()));
+            uci.start_with(args.map(|a| FallbackStr(a.into_string().ok())));
         },
     }
 }
