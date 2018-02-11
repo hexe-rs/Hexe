41507397ddb24df7ec5bbc3d2bcebbcf78d3f35c
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-02-10 23:08:04 -0500

    Run commands from arguments by default in hexe_bin

--- hexe_bin/src/main.rs
+++ hexe_bin/src/main.rs
@@ -1,5 +1,17 @@
 extern crate hexe;
 
+use std::env;
+
 fn main() {
-    hexe::engine::Engine::default().uci().start();
+    let mut args = env::args();
+    args.next();
+
+    let mut eng = hexe::engine::Engine::default();
+    let mut uci = eng.uci();
+
+    if args.len() == 0 {
+        uci.start();
+    } else {
+        uci.start_with(args);
+    }
 }
