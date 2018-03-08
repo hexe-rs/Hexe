326dbc2a5ac1734a9af8442a78606f8451b8306f
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-03-08 14:47:48 -0500

    Simplify trailing pattern in bitboard masks macro

--- hexe_core/src/board/bitboard/masks.rs
+++ hexe_core/src/board/bitboard/masks.rs
@@ -5,9 +5,9 @@
 use super::*;
 
 macro_rules! impl_consts {
-    ($base:expr, $shift:expr; $cur:ident, $($next:ident),+ $(,)*) => {
+    ($base:expr, $shift:expr; $cur:ident, $($rest:tt)+) => {
         pub const $cur: Bitboard = Bitboard($base);
-        impl_consts!($shift; $cur, $($next),+);
+        impl_consts!($shift; $cur, $($rest)+);
     };
     ($shift:expr; $prev:ident, $cur:ident) => {
         pub const $cur: Bitboard = Bitboard($prev.0 << $shift);
@@ -20,12 +20,10 @@ macro_rules! impl_consts {
 
 impl_consts! {
     0x0101010101010101, 1;
-    FILE_A, FILE_B, FILE_C, FILE_D,
-    FILE_E, FILE_F, FILE_G, FILE_H,
+    FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H
 }
 
 impl_consts! {
     0xFF, 8;
-    RANK_1, RANK_2, RANK_3, RANK_4,
-    RANK_5, RANK_6, RANK_7, RANK_8,
+    RANK_1, RANK_2, RANK_3, RANK_4, RANK_5, RANK_6, RANK_7, RANK_8
 }
