55a3b0a4e12fa623ef68a9af4231fd1f0746aebf
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-04-06 16:58:36 -0400

    Properly handle shrinking the hash table

--- hexe/src/table.rs
+++ hexe/src/table.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::mem;
 
 const CACHE_LINE:   usize = 64;
@@ -51,19 +52,20 @@ impl Table {
     pub fn resize_exact(&mut self, size_mb: usize) {
         let new = size_mb * MB_SIZE / CLUSTER_SIZE;
         let old = self.clusters.len();
-        if new == old {
-            return;
-        }
 
-        if new > old {
-            self.clusters.reserve_exact(new - old);
-            unsafe {
+        match new.cmp(&old) {
+            Ordering::Equal => return,
+            Ordering::Greater => unsafe {
+                self.clusters.reserve_exact(new - old);
+                self.clusters.set_len(new);
                 let slice = self.clusters.get_unchecked_mut(old..new);
                 ::util::zero(slice);
-            }
+            },
+            Ordering::Less => {
+                self.clusters.truncate(new);
+                self.clusters.shrink_to_fit();
+            },
         }
-
-        unsafe { self.clusters.set_len(new) };
     }
 
     /// Zeroes out the entire table.
