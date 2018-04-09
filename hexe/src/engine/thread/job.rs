c716ff94f5052a9bae8501b8f1c2bf2ea55ed1c4
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-04-09 13:36:41 -0400

    Reorganize thread::Pool

---
+++ hexe/src/engine/thread/job.rs
@@ -0,0 +1,7 @@
+pub struct Job;
+
+impl Job {
+    pub fn execute(&self, thread: usize) {
+        unimplemented!("Can't yet execute on thread {}", thread);
+    }
+}
--- hexe/src/engine/thread/mod.rs
+++ hexe/src/engine/thread/mod.rs
@@ -3,31 +3,34 @@ use std::thread::{self, JoinHandle};
 
 use crossbeam_deque::{Deque, Stealer, Steal};
 
-pub struct Thread {
+mod job;
+pub use self::job::Job;
+
+struct Thread {
     /// The index for this thread.
     index: usize,
     /// Join up with everyone else.
     handle: JoinHandle<()>,
 }
 
-pub struct Job;
-
-impl Job {
-    fn execute(&self, thread: usize) {
-        unimplemented!("Can't yet execute on thread {}", thread);
-    }
-}
-
-pub struct Pool {
+struct PoolInner {
     /// All threads spawned within this pool.
     threads: Vec<Thread>,
+}
+
+#[cfg(test)]
+assert_impl!(inner; PoolInner, Send, Sync);
+
+pub struct Pool {
+    /// An inner thread pool that must
+    inner: PoolInner,
     /// Insertion point for jobs.
     jobs: Deque<Job>,
 }
 
 impl Drop for Pool {
     fn drop(&mut self) {
-        for thread in self.threads.drain(..) {
+        for thread in self.inner.threads.drain(..) {
             if let Err(_) = thread.handle.join() {
                 unreachable!("Thread panicked");
             }
@@ -38,14 +41,14 @@ impl Drop for Pool {
 impl Pool {
     /// Creates a new pool with `n` number of threads.
     pub fn new(n: usize) -> Pool {
-        let mut pool = Pool {
+        let mut pool = PoolInner {
             threads: Vec::with_capacity(n),
-            jobs: Deque::new(),
         };
+        let jobs = Deque::<Job>::new();
 
         for index in 0..n {
             // Request stealer while in pool thread
-            let stealer = pool.jobs.stealer();
+            let stealer = jobs.stealer();
 
             let handle = thread::spawn(move || {
                 // Move the stealer into worker scope
@@ -68,16 +71,16 @@ impl Pool {
             });
         }
 
-        pool
+        Pool { inner: pool, jobs }
     }
 
     /// Returns the number of threads in the pool.
     pub fn num_threads(&self) -> usize {
-        self.threads.len()
+        self.inner.threads.len()
     }
 
-    /// A deque of jobs are available to the pool.
-    pub fn jobs(&self) -> &Deque<Job> {
-        &self.jobs
+    /// Enqueues the job to be executed.
+    pub fn enqueue(&self, job: Job) {
+        self.jobs.push(job);
     }
 }
