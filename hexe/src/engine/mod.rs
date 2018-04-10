1e809ee41135f6a59882fc0759aad5989b9ca87c
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-04-09 20:04:00 -0400

    Restructure Engine internals

--- hexe/src/engine/mod.rs
+++ hexe/src/engine/mod.rs
@@ -5,7 +5,6 @@
 
 use std::usize;
 
-use position::Position;
 use table::Table;
 
 mod limits;
@@ -35,8 +34,6 @@ pub use self::uci::Uci;
 pub struct Engine {
     pool: Pool,
     table: Table,
-    position: Position,
-    options: Options,
 }
 
 impl Default for Engine {
@@ -48,16 +45,25 @@ impl Engine {
     /// Creates a new builder for setting engine options.
     #[inline]
     pub fn builder() -> EngineBuilder {
-        EngineBuilder {
+        EngineBuilder(Options {
             num_threads: 0,
             hash_size: 0,
-        }
+        })
     }
 
     /// Creates an instance of the engine with the default options applied.
     #[inline]
     pub fn new() -> Engine {
         Engine::builder().build()
+    }
+
+    /// The internal options used by this instance.
+    #[inline]
+    pub fn options(&self) -> Options {
+        Options {
+            num_threads: self.num_threads(),
+            hash_size: self.table.size_mb(),
+        }
     }
 
     /// Creates a Universal Chess Interface for this engine.
@@ -80,27 +86,29 @@ impl Engine {
 
 /// A type that can be used to build an [`Engine`](struct.Engine.html) instance.
 #[derive(Copy, Clone, Debug)]
-pub struct EngineBuilder {
-    num_threads: usize,
-    hash_size: usize,
+pub struct EngineBuilder(Options);
+
+impl From<Options> for EngineBuilder {
+    #[inline]
+    fn from(opt: Options) -> EngineBuilder {
+        EngineBuilder(opt)
+    }
 }
 
 impl EngineBuilder {
     /// Builds a new [`Engine`](struct.Engine.html) with the options of `self`.
     pub fn build(self) -> Engine {
-        let num_threads = match self.num_threads {
+        let num_threads = match self.0.num_threads {
             0 => ::num_cpus::get(),
             n => n,
         };
-        let hash_size = match self.hash_size {
+        let hash_size = match self.0.hash_size {
             0 => 1,
             n => n,
         };
         Engine {
-            pool:     Pool::new(num_threads),
-            table:    Table::new(hash_size, true),
-            position: Position::default(),
-            options:  Options { num_threads },
+            pool:  Pool::new(num_threads),
+            table: Table::new(hash_size, true),
         }
     }
 
@@ -111,22 +119,26 @@ impl EngineBuilder {
     /// logical CPUs.
     #[inline]
     pub fn num_threads(mut self, n: usize) -> EngineBuilder {
-        self.num_threads = n;
+        self.0.num_threads = n;
         self
     }
 
-    /// The number of megabytes available for the hash table.
+    /// The number of megabytes available for the transposition table.
     ///
     /// If `n` is 0, or you do not call this function, then the table size will
     /// be selected automatically. The default is 1.
     #[inline]
     pub fn hash_size(mut self, size_mb: usize) -> EngineBuilder {
-        self.hash_size = size_mb;
+        self.0.hash_size = size_mb;
         self
     }
 }
 
 /// Chess engine options.
-struct Options {
-    num_threads: usize,
+#[derive(Copy, Clone, Debug)]
+pub struct Options {
+    /// The number of threads in the engine's thread pool.
+    pub num_threads: usize,
+    /// The number of megabytes for the engine's transposition table.
+    pub hash_size: usize,
 }
