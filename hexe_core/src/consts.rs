284b1981c154ef06ba8bdd0103844b8065f5206f
Nikolai Vazquez <nvazquez1297@gmail.com>
2018-04-14 09:41:28 -0400

    Use mem::size_of for PTR_SIZE value

--- hexe_core/src/consts.rs
+++ hexe_core/src/consts.rs
@@ -1,8 +1,6 @@
-#[cfg(target_pointer_width = "64")]
-pub const PTR_SIZE: usize = 8;
+use core::mem;
 
-#[cfg(target_pointer_width = "32")]
-pub const PTR_SIZE: usize = 4;
+pub const PTR_SIZE: usize = mem::size_of::<usize>();
 
 pub const BOARD_DOTS: &[u8; 127] = b". . . . . . . .\n\
                                      . . . . . . . .\n\
